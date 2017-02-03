88fa143c2ba7c73cdcc5a56b44b53376c151698a
Leif Sawyer <ak.hepcat@gmail.com>
2017-02-03 09:54:04 -0900

    refactored, added sanity checks, additional blacklist sources

--- v4-blacklist.sh
+++ v4-blacklist.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-IP_TMP=/tmp/ip.tmp
-IP_BLACKLIST=/etc/ip-blacklist.conf
-IP_BLACKLIST_TMP=/tmp/ip-blacklist.tmp
-IP_BLACKLIST_CUSTOM=/etc/ip-blacklist-custom.conf # optional
-list="chinese nigerian russian lacnic exploited-servers"
+#  based on the original script from https://n0where.net/iptables-blacklist-script/
+#  additional lists from simestd
+#  refactoring by ak_hepcat
+PROG="${0##*/}"
+DO_RETURN=${SHLVL}
+
+IP4_BLACKLIST_GEN=/etc/ip-blacklist.conf
+IP4_BLACKLIST_CUSTOM=/etc/ip-blacklist-custom.conf # optional
+
+BL_SET=blacklist	# name of your ipset hash, in case of multiples
+
+### Blacklists - remember, maximum number of sorted entries is 65,535 (2^16)
+WIZLISTS="chinese nigerian russian lacnic exploited-servers" #wizcraft lists
 BLACKLISTS=(
 "https://reputation.alienvault.com/reputation.snort.gz"
 "http://list.iblocklist.com/?list=dgxtneitpuvgqqcpfulq&fileformat=p2p&archiveformat=gz"
@@ -24,26 +32,77 @@ BLACKLISTS=(
 "http://www.autoshun.org/files/shunlist.csv" # Autoshun Shun List
 "http://lists.blocklist.de/lists/all.txt" # blocklist.de attackers
 )
-for i in "${BLACKLISTS[@]}"
+
+###
+
+trap cleanup SIGINT SIGTERM SIGKILL SIGQUIT SIGABRT SIGSTOP SIGSEGV
+
+do_exit()
+{
+        STATUS=${1:-0}
+        REASON=${2}
+
+        [[ -n "${REASON}" ]] && echo "${REASON}"
+
+        [[ ${DO_RETURN} -eq 1 ]] && return $STATUS || exit $STATUS
+}
+
+cleanup() {
+	rm -f ${IP4_BLACKLIST_T}
+	rm -f ${IPSET_T}
+	do_exit
+}
+
+prerequisites() {
+
+	if [ "$(id | cut -c1-5)" != "uid=0" ] ; then
+	  echo "You must be the root user to run ${PROG}"
+	  do_exit 1
+	fi
+
+	if [ -z "$(which ipset)" ]; then
+	  echo "ipset required but not found.  Please install before continuing"
+	  do_exit 1
+	fi
+	
+	if [ -z "$(ipset list -n)" ]; then
+	  ipset create ${BL_SET} hash:net
+	fi
+
+	if [ -z "$(iptables -L -n | grep -iE \"match-set.*${BL_SET}\" )" ]; then
+	  echo "ipset rule not found in current iptables for blacklist: ${BL_SET}"
+	  echo "insert the following rule where appropriate before running this script"
+	  echo "    -A INPUT -m set --match-set ${BL_SET} src -j DROP"
+	  do_exit 1
+	fi
+}
+
+#############################
+
+prerequisites()
+
+IP4_BLACKLIST_T=$(mktemp /tmp/ip4_bl-XXXXXX.tmp)
+
+for list in ${BLACKLISTS[@]}
 do
-    curl "$i" > $IP_TMP
-    zgrep -Po '(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?' $IP_TMP >> $IP_BLACKLIST_TMP
-done
-for i in `echo $list`; do
-        # Download
-        wget --quiet http://www.wizcrafts.net/$i-iptables-blocklist.html
-        # Grep out all but ip blocks
-        cat $i-iptables-blocklist.html | grep -v \< | grep -v \: | grep -v \; | grep -v \# | grep [0-9] > $i.txt
-        # Consolidate blocks into master list
-        cat $i.txt >> $IP_BLACKLIST_TMP
+    curl "${list}" | zgrep -Po '(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?'
+done >> ${IP4_BLACKLIST_T}
+
+for list in ${WIZLISTS}; do
+        curl "http://www.wizcrafts.net/${list}-iptables-blocklist.html" | grep -v \< | grep -v \: | grep -v \; | grep -v \# | grep [0-9]
+done >> ${IP4_BLACKLIST_T}
+
+sort -n ${IP4_BLACKLIST_T} ${IP4_BLACKLIST_CUSTOM} | uniq > ${IP4_BLACKLIST_GEN}
+
+LINES=$(wc -l ${IP4_BLACKLIST_GEN} | awk '{print $1}')
+if [ ${LINES} -gt 65535 ]; then
+	do_exit 1 "Exceeded max IP entries for ipset"
+fi
+
+ipset flush ${BL_SET}
+egrep -v "^#|^$" ${IP4_BLACKLIST_GEN} | while IFS= read -r IP
+do
+        ipset add ${BL_SET} ${IP}
 done
 
-sort $IP_BLACKLIST_TMP -n | uniq > $IP_BLACKLIST
-rm $IP_BLACKLIST_TMP
-wc -l $IP_BLACKLIST
-
-ipset flush blacklist
-egrep -v "^#|^$" $IP_BLACKLIST | while IFS= read -r ip
-do
-        ipset add blacklist $ip
-done
+cleanup()
