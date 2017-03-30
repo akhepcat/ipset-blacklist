d05101c7a417a03919bae20a9567cdbc1f9f9bb2
Leif Sawyer <ak.hepcat@gmail.com>
2017-03-30 11:19:03 -0800

    call them IPv4 addresses

--- ipmerge.pl
+++ ipmerge.pl
@@ -5,7 +5,7 @@
 # Author: Royce Williams
 # Updated-Date: 2017-03-30
 # Updated-By: Leif Sawyer
-# Purpose: combine a list of subnets into the largest possible contiguous blocks.
+# Purpose: combine a list of IPv4 subnets into the largest possible contiguous blocks.
 # $Id: ipmerge.pl,v 1.1 2014/05/02 17:43:32 royce Exp royce $
 #-----------------------------------------------------------------------
 
@@ -19,7 +19,7 @@ use NetAddr::IP qw(Compact);
 my @addresses;
 my @addresses_raw;
 
-# Filter out non-IP records.
+# Filter out non-IPv4 records.
 while (<STDIN>) {
     if (m/^((?:\d{1,3}\.){3}\d{1,3}(?:\/\d{1,2})?)(.*)$/) {
         push @addresses_raw, $1;
