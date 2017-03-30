95f70536a0ec0c862737de4abd2e94f88f7eb9fe
Leif Sawyer <ak.hepcat@gmail.com>
2017-03-30 11:10:11 -0800

    fix up the regex on ipmerge so we ignore non IPv4 data

--- ipmerge.pl
+++ ipmerge.pl
@@ -3,6 +3,8 @@
 # Name: ipmerge.pl
 # Created: 2014-04-16
 # Author: Royce Williams
+# Updated-Date: 2017-03-30
+# Updated-By: Leif Sawyer
 # Purpose: combine a list of subnets into the largest possible contiguous blocks.
 # $Id: ipmerge.pl,v 1.1 2014/05/02 17:43:32 royce Exp royce $
 #-----------------------------------------------------------------------
@@ -15,14 +17,17 @@ use NetAddr::IP qw(Compact);
 #-----------------------------------------------------------------------
 
 my @addresses;
-my @addresses_raw = (<STDIN>);
+my @addresses_raw;
 
-# Rough filter for non-IP records.
-my @addresses_raw2 = grep /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(\/\d{1,2})?/, @addresses_raw;
+# Filter out non-IP records.
+while (<STDIN>) {
+    if (m/^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(\/\d{1,2})?)(.*)$/) {
+        push @addresses_raw, $1;
+    }
+}
 
 # Create network objects for each record.
-chomp(@addresses_raw2);
-push @addresses, NetAddr::IP->new($_) for @addresses_raw2;
+push @addresses, NetAddr::IP->new($_) for @addresses_raw;
 
 # Print output.
 print join("\n", Compact(@addresses)), "\n";
